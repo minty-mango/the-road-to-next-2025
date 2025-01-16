17110c93c8458e6fbccfbf77809f81d84dbb14bc
Michael Sasota <50548722+minty-mango@users.noreply.github.com>
2025-01-16 14:47:32 -0500

    Change to files about cookies

--- src/features/ticket/actions/delete-tickets.ts
+++ src/features/ticket/actions/delete-tickets.ts
@@ -2,16 +2,18 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { setCookiesByKey } from "@/actions/cookies";
 import { prisma } from "@/lib/prisma";
 import { ticketsPath } from "@/paths";
 
 export const deleteTicket = async (id: string) => {
-    "use server";
+  "use server";
 
-    await prisma.ticket.delete({
-        where: { id: id }
-    });
+  await prisma.ticket.delete({
+    where: { id: id },
+  });
 
-    revalidatePath(ticketsPath());
-    redirect(ticketsPath());
+  revalidatePath(ticketsPath());
+  setCookiesByKey("toast", "Ticket deleted");
+  redirect(ticketsPath());
 };
